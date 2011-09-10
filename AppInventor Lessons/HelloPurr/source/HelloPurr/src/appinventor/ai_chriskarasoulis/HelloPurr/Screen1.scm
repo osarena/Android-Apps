da3e38b0f69cba79eedcf92d91e64efce0bdd0c9
Salih Emin <salih.emin.vet@gmail.com>
2011-09-10 20:57:54 +0300

    κώδικας και εφαρμογή απο το πρώτο μάθημα προγραμματισμού στο android

---
+++ AppInventor Lessons/HelloPurr/source/HelloPurr/src/appinventor/ai_chriskarasoulis/HelloPurr/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"51","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"6","Uuid":"0","Title":"\u0393\u03b5\u03b9\u03b1 \u03c3\u03bf\u03c5 \u03b3\u03b1\u03c4\u03ac\u03ba\u03b9","$Components":[{"$Name":"Button1","$Type":"Button","$Version":"3","Uuid":"271788351","Image":"kitty.png"},{"$Name":"Label1","$Type":"Label","$Version":"2","Uuid":"2061189818","Text":"\u03a7\u03ac\u03b9\u03b4\u03b5\u03c8\u03b5 \u03c4\u03b7 \u03b3\u03ac\u03c4\u03b1"},{"$Name":"Sound1","$Type":"Sound","$Version":"3","Uuid":"-1759032096","Source":"meow.mp3"}]}}
+|#
