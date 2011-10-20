1739146c63d07de06310456596de628afa4f9704
Salih Emin <salih.emin.vet@gmail.com>
2011-10-21 01:17:56 +0300

    Νεα αρχεία για το μάθημα AppInventor

---
+++ AppInventor Lessons/PicCall/source/src/appinventor/ai_chriskarasoulis/PicCall/Screen1.scm
@@ -0,0 +1,4 @@
+#|
+$JSON
+{"YaVersion":"51","Source":"Form","Properties":{"$Name":"Screen1","$Type":"Form","$Version":"6","Uuid":"0","Title":"PicCall","$Components":[{"$Name":"Button1","$Type":"Button","$Version":"3","Uuid":"1895775162","Image":"mj.jpeg","Width":"150","Height":"150"},{"$Name":"PhoneNumberPicker1","$Type":"PhoneNumberPicker","$Version":"3","Uuid":"1320987465","Text":"\u0394\u03b9\u03ac\u03bb\u03b5\u03be\u03b5 \u03bc\u03b9\u03b1 \u03b5\u03c0\u03b1\u03c6\u03ae"},{"$Name":"PhoneCall1","$Type":"PhoneCall","$Version":"1","Uuid":"-555619915","PhoneNumber":"1234567890"}]}}
+|#
